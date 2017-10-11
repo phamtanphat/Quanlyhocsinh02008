25103eb15a28d07e013e1cf11e7c60ea16dea51a
thuviengithub0208 <khoaphp@yahoo.com>
2017-10-11 19:16:46 +0700

    "Finish"

--- app/src/main/java/com/ptp/phamtanphat/quanlyhocsinh02008/Server.java
+++ app/src/main/java/com/ptp/phamtanphat/quanlyhocsinh02008/Server.java
@@ -8,7 +8,7 @@ public class Server {
 
     public static final String http = "http://";
 
-    public static final String localhost = http + "192.168.1.181/Quanlyhocsinh/";
+    public static final String localhost = http + "phatdroid94com.000webhostapp.com/Quanlyhocsinh/";
     public static final String GetData = localhost + "getdata.php";
     public static final String InsertData = localhost + "insertdata.php";
     public static final String UpdateData = localhost + "update.php";
